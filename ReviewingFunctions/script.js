82cc43b867aa19b20a39ad16a5647e4768a6394c
Ayush Katoch <katochayushhp@gmail.com>
2022-05-26 00:22:22 +0530

    Update script.js

--- ReviewingFunctions/script.js
+++ ReviewingFunctions/script.js
@@ -1,17 +1,23 @@
 'use strict';
 
-function cutFruitPieces(fruits) {
-    return fruits *2;
+const calcAge = function(Year) {
+    return 2022 - Year;
 }
 
-function fruitProcessor(apples, oranges) {
-    const cutApples = cutFruitPieces(apples);
-    const cutOranges = cutFruitPieces(oranges);
+const yearsUntilRetirement = function(birthYear, name) {
+    const age = calcAge(birthYear);
 
-    const juice = `Juice is being made of ${cutApples} apple and 
-                     ${cutOranges} orange pieces`;
-            
-                  return juice;
+    const retirementYears = 60 - age;
+
+    if(retirementYears > 0) {
+        console.log(retirementYears);
+        return `${name} will retire in ${retirementYears} years`;
+    } else {
+        console.log("Already retired");
+        return -1;
+    }
+
 }
 
-console.log(fruitProcessor(8, 10));
+console.log(yearsUntilRetirement(2002, "Ayush"));
+console.log(yearsUntilRetirement(1970, "Ayon"));
