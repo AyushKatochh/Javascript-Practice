7b63b2151f80220f54f2e7ea8abd334aac55d9d6
Ayush Katoch <katochayushhp@gmail.com>
2022-05-26 01:09:33 +0530

    Create script.js

---
+++ arrays/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const friends  = ["Ayush", "Ayon", "Angelee"];
+
+const years = new Array(2002, 2003, 2004);
+
+console.log(friends);
+console.log(years);
+
+console.log(friends[2]);
+console.log(friends.length);
+
+//Passing only function expressions
+console.log(friends[friends.length - 1]);
