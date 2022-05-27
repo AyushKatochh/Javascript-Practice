0cb4c319fd92fc5171b729485540adafc4c9b317
Ayush Katoch <katochayushhp@gmail.com>
2022-05-27 22:57:43 +0530

    Update script.js

--- arrays/script.js
+++ arrays/script.js
@@ -12,3 +12,21 @@ console.log(friends.length);
 
 //Passing only function expressions
 console.log(friends[friends.length - 1]);
+
+friends[2] = "Angelina";
+console.log(friends);
+
+const firstName = "Katoch";
+
+const Ayush = [firstName, "Ayush", 2022-2002, "Student", friends];
+
+console.log(Ayush);
+
+const calcAge = function (birthYear) {
+    return 2022 - birthYear;
+}
+
+const year = [2001, 2002, 2003];
+
+console.log(calcAge(year[0]));
+console.log(calcAge(year[1]));
