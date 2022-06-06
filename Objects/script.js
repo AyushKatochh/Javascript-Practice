d6a8851a2f20a1879002d0af892edbd8ab05fd48
Ayush Katoch <katochayushhp@gmail.com>
2022-06-07 00:11:39 +0530

    Create script.js

---
+++ Objects/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const objectIntroduction = {
+    firstName: "Ayush",
+    lastName: "Katoch",
+    age: 2022 - 2002,
+    job: "Software Engineer",
+    friends: ["Ayon", "Angelee", "Sheetal", "Sanchi", "Manogna"]
+}
+
+console.log(objectIntroduction.firstName);
+
+console.log(objectIntroduction['firstName']);
+
+const nameKey = "Name";
+
+console.log(objectIntroduction['first' + nameKey]);
+
+const interestedIn = prompt("firstName, lastName, age, job, firends");
+console.log(objectIntroduction[interestedIn]);
+
+if(objectIntroduction[interestedIn]) {
+    objectIntroduction[interestedIn];
+} else {
+    console.log("Wrong Request");
+}
+
+console.log(`${objectIntroduction.firstName} has ${objectIntroduction.friends.length} friends and his best friend is ${objectIntroduction.friends[0]}`);
