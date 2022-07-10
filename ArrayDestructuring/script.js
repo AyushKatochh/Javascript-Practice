d85b7e77462c045414daf7c34a125ffb4d756515
Ayush Katoch <katochayushhp@gmail.com>
2022-07-10 23:01:03 +0530

    Create script.js

---
+++ ArrayDestructuring/script.js
@@ -0,0 +1,46 @@
+'use strict';
+
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+  order: function(starterIndex, mainIndex) {
+     return `${this.starterMenu[starterIndex]} ${this.mainMenu[mainIndex]}`;
+  }
+};
+
+let [first, second] = restaurant.categories;
+console.log(first, second);
+
+[second, first] = [first, second];
+console.log(first, second);
+
+console.log(restaurant.order(1,2));
+
+const nested = [1,2,[3,4]];
+
+const [i, , [j,k]] = nested;
+
+console.log(i,j,k);
