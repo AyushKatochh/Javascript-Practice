eb858c9f2b19d1e945f307de4c2a4e613b81f3d0
Ayush Katoch <katochayushhp@gmail.com>
2022-07-16 11:27:18 +0530

    Update script.js

--- Map/script.js
+++ Map/script.js
@@ -22,3 +22,26 @@ rest.set(arrayForKey, 'Sonal');
 console.log(rest.get(arrayForKey));
 const setDOM = rest.set(document.querySelector('h1'), "Selected");
 console.log(setDOM);
+
+//Map Iteration
+
+const question = new Map([
+    ['question', 'What is the best programming language in the world'],
+    [1, 'C'],
+    [2, 'Java'],
+    [3, 'Javascript'],
+    ['correct', 3],
+    [true, 'Correct'],
+    [false, 'Try Again']
+])
+
+console.log(question.get('question'));
+
+for(const [key, value] of question) {
+    if(typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+} 
+
+const answer = Number(prompt("Your Answer"));
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
