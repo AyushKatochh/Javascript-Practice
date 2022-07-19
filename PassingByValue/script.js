fe17de351d8c268983764325982c0cd39ee7da55
Ayush Katoch <katochayushhp@gmail.com>
2022-07-19 10:59:55 +0530

    Update script.js

--- PassingByValue/script.js
+++ PassingByValue/script.js
@@ -27,3 +27,31 @@ const newPassport = function(person) {
 
 newPassport(ayush);
 checkIn(flight, ayush);
+
+// Functions calling other functions
+const oneWord = function(str) {
+    return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function(str) {
+    const [first, ...others] = str.split(' ');
+    return [first.toUpperCase(), ...others].join(' ');
+}
+
+//Higher-order function
+const transformer = function (str, fn) {
+    console.log(`Original string: ${str}`);
+    console.log(`Transformed string: ${fn(str)}`);
+
+    console.log(`Transformed by: ${fn.name}`);
+}
+
+transformer('Javascript is the best!', upperFirstWord);
+transformer('Javascript is the best', oneWord);
+
+const high5 = function() {
+    console.log('👍');
+}
+
+document.body.addEventListener('click', high5);
+
